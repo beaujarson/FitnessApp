29730928cea4cd78808b8ed106b66aaac94ff699
DrScatman <milleja1@mail.gvsu.edu>
2019-12-02 14:34:32 -0500

    Jake

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,115 +1,162 @@
 <template>
   <div id="page">
-      <div id="workoutForm">
-        <v-select
-          v-model="pushExercise"
-          v-bind:items="pushCategories"
-          label="Push Exercise"
-        ></v-select>
-        <v-select
-          v-model="pullExercise"
-          v-bind:items="pullCategories"
-          label="Pull Exercise"
-        ></v-select>
-        <v-select
-          v-model="legExercise"
-          v-bind:items="legCategories"
-          label="Leg Exercise"
-        ></v-select>
-        <v-text-field label="Sets" type="number" step="1.0" v-model.number="sets" />
-        <v-text-field label="Reps" type="number" step="1.0" v-model.number="reps" />
-        <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                    <v-btn color="primary" @click="remove" v-bind:disabled="userSelections.length == 0" v-on="on">Remove Selection(s)</v-btn>
-                </template>
-                <span>Remove the selected items from the table.</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                    <v-btn color="primary" @click="yourButtonHandler" v-on="on">Add</v-btn>
-                </template>
-                <span>Add item to the table.</span>
-        </v-tooltip>
-      </div>
+    <div id="workoutForm">
+      <v-select
+        v-model="pushExercise"
+        v-bind:items="pushCategories"
+        label="Push Exercise"
+      ></v-select>
+      <v-select
+        v-model="pullExercise"
+        v-bind:items="pullCategories"
+        label="Pull Exercise"
+      ></v-select>
+      <v-select
+        v-model="legExercise"
+        v-bind:items="legCategories"
+        label="Leg Exercise"
+      ></v-select>
+      <v-text-field
+        label="Sets"
+        type="number"
+        step="1.0"
+        v-model.number="sets"
+      />
+      <v-text-field
+        label="Reps"
+        type="number"
+        step="1.0"
+        v-model.number="reps"
+      />
+      <v-select
+        v-model="privacy"
+        v-bind:items="privacyCategories"
+        label="Privacy"
+      ></v-select>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            color="primary"
+            @click="remove"
+            v-bind:disabled="userSelections.length == 0"
+            v-on="on"
+            >Remove Selection(s)</v-btn
+          >
+        </template>
+        <span>Remove the selected items from the table.</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" @click="yourButtonHandler" v-on="on"
+            >Add</v-btn
+          >
+        </template>
+        <span>Add item to the table.</span>
+      </v-tooltip>
+    </div>
 
-  <p id="one">Enter your workout into the form and it will show up in both the "Your workouts" table and the "All users workouts" table. Comparing your workouts to other users can help you craft a workout routine that best suits your needs.</p>
+    <p id="one">
+      Enter your workout into the form and it will show up in both the "Your
+      workouts" table and the "All users workouts" table. Comparing your
+      workouts to other users can help you craft a workout routine that best
+      suits your needs.
+    </p>
 
-  <v-simple-table id="publicTable" fixed-header >
-    <div id="th">| Your workouts |</div>
-    <table>
-      <thead>
-        <tr>
-        <th id="th">Push Exercise</th>
-        <th id="th">Pull Exercise</th>
-        <th id="th">Leg Exercise</th>
-        <th id="th">Sets</th>
-        <th id="th">Reps</th>
-        <th id="th">Selection</th></tr>
-      </thead>
-      <tbody>
-        <tr id="dataRows" v-for="(myWorkout,pos) in myWorkout" :key="pos">
-          <td>{{ myWorkout.Push }}</td>
-          <td>{{ myWorkout.Pull }}</td>
-          <td>{{ myWorkout.Legs }}</td>
-          <td id="sets">{{ myWorkout.Sets }}</td>
-          <td id="reps">{{ myWorkout.Reps }}</td>
-          <td><input type="checkbox" v-bind:id="myWorkout.mykey" v-on:change="selectionHandler" /></td>
-        </tr>
-      </tbody>
-    </table>
-  </v-simple-table>
-        
     <v-simple-table id="publicTable" fixed-header>
-        <div id="th">| All users workouts |</div>
-    <table>
-      <thead>
-        <tr>
-        <th id="th">User</th>
-        <th id="th">Push Exercise</th>
-        <th id="th">Pull Exercise</th>
-        <th id="th">Leg Exercise</th>
-        <th id="th">Sets</th>
-        <th id="th">Reps</th></tr>
-      </thead>
-      <tbody>
-        <tr id="dataRows" v-for="(myWorkout,pos) in myWorkout" :key="pos">
-          <td>{{ myWorkout.userEmail }}</td>
-          <td>{{ myWorkout.Push }}</td>
-          <td>{{ myWorkout.Pull }}</td>
-          <td>{{ myWorkout.Legs }}</td>
-          <td id="sets">{{ myWorkout.Sets }}</td>
-          <td id="reps">{{ myWorkout.Reps }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </v-simple-table>
+      <div id="th">| Your workouts |</div>
+      <table>
+        <thead>
+          <tr>
+            <th id="th">Push Exercise</th>
+            <th id="th">Pull Exercise</th>
+            <th id="th">Leg Exercise</th>
+            <th id="th">Sets</th>
+            <th id="th">Reps</th>
+            <th id="th">Selection</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr id="dataRows" v-for="(myWorkout, pos) in myWorkout" :key="pos">
+            <td>{{ myWorkout.Push }}</td>
+            <td>{{ myWorkout.Pull }}</td>
+            <td>{{ myWorkout.Legs }}</td>
+            <td id="sets">{{ myWorkout.Sets }}</td>
+            <td id="reps">{{ myWorkout.Reps }}</td>
+            <td>
+              <input
+                type="checkbox"
+                v-bind:id="myWorkout.mykey"
+                v-on:change="selectionHandler"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </v-simple-table>
 
+    <v-simple-table id="publicTable" fixed-header>
+      <div id="th">| All users workouts |</div>
+      <table>
+        <thead>
+          <tr>
+            <th id="th">User</th>
+            <th id="th">Push Exercise</th>
+            <th id="th">Pull Exercise</th>
+            <th id="th">Leg Exercise</th>
+            <th id="th">Sets</th>
+            <th id="th">Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr id="dataRows" v-for="(myWorkout, pos) in myWorkout" :key="pos">
+            <td>{{ myWorkout.userEmail }}</td>
+            <td>{{ myWorkout.Push }}</td>
+            <td>{{ myWorkout.Pull }}</td>
+            <td>{{ myWorkout.Legs }}</td>
+            <td id="sets">{{ myWorkout.Sets }}</td>
+            <td id="reps">{{ myWorkout.Reps }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </v-simple-table>
   </div>
-  
 </template>
 
 <script>
 import { AppDB } from "../db-init.js";
 
-
-  export default {
+export default {
   data() {
     return {
-      pushCategories : ["Bench press", "Shoulder press", "Chest-Fly", "Triceps", "Push-ups"],
-      pullCategories : ["Row", "Pull-ups", "Pull-downs", "Shrug", "Dead lift"],
-      legCategories : ["Squat", "Romanian deadlift", "Lunge", "Calf-raises", "Leg press"],
-      userSelections : [],
+      pushCategories: [
+        "Bench press",
+        "Shoulder press",
+        "Chest-Fly",
+        "Triceps",
+        "Push-ups"
+      ],
+      pullCategories: ["Row", "Pull-ups", "Pull-downs", "Shrug", "Dead lift"],
+      legCategories: [
+        "Squat",
+        "Romanian deadlift",
+        "Lunge",
+        "Calf-raises",
+        "Leg press"
+      ],
+      privacyCategories: ["Private", "Public"],
+      userSelections: [],
       myWorkout: [],
       pushExercise: "",
       pullExercise: "",
       legExercise: "",
+      privacy: "",
+      userEmail: "",
       sets: 0,
       reps: 0
     };
   },
 
   methods: {
-    
     removeExpenseItem(snapshot) {
       /* snapshot.key will hold the key of the item being REMOVED */
       this.myWorkout = this.myWorkout.filter(z => z.mykey != snapshot.key);
@@ -118,53 +165,66 @@ import { AppDB } from "../db-init.js";
 
     dataHandler(snapshot) {
       const item = snapshot.val();
-      this.myWorkout.push({...item, mykey: snapshot.key});
+      this.myWorkout.push({ ...item, mykey: snapshot.key });
     },
 
     yourButtonHandler() {
-      AppDB.ref("workoutPrivate")
-      .push()
-      .set({
-      Push: this.pushExercise,
-      Pull: this.pullExercise,
-      Legs: this.legExercise,
-      Sets: this.sets,
-      Reps: this.reps
-      });
-      AppDB.ref("workoutPublic")
-      .push()
-      .set({
-      Push: this.pushExercise,
-      Pull: this.pullExercise,
-      Legs: this.legExercise,
-      Sets: this.sets,
-      Reps: this.reps
-      });
+      if (this.privacy.endsWith("e")) {
+        AppDB.ref("workoutPrivate")
+          .push()
+          .set({
+            Push: this.pushExercise,
+            Pull: this.pullExercise,
+            Legs: this.legExercise,
+            Sets: this.sets,
+            Reps: this.reps
+          });
+      } else {
+        AppDB.ref("workoutPrivate")
+          .push()
+          .set({
+            Push: this.pushExercise,
+            Pull: this.pullExercise,
+            Legs: this.legExercise,
+            Sets: this.sets,
+            Reps: this.reps
+          });
+        AppDB.ref("workoutPublic")
+          .push()
+          .set({
+            Email: this.userEmail,
+            Push: this.pushExercise,
+            Pull: this.pullExercise,
+            Legs: this.legExercise,
+            Sets: this.sets,
+            Reps: this.reps
+          });
+      }
     },
 
     remove() {
-      this.userSelections.forEach((victimKey) => {
+      this.userSelections.forEach(victimKey => {
         AppDB.ref("workoutPrivate")
-        .child(victimKey)
-        .remove();
-    });
-      this.userSelections.forEach((victimKey) => {
+          .child(victimKey)
+          .remove();
+      });
+      this.userSelections.forEach(victimKey => {
         AppDB.ref("workoutPublic")
-        .child(victimKey)
-        .remove();
-    });
+          .child(victimKey)
+          .remove();
+      });
     },
 
-    selectionHandler (changeEvent) {
-    const whichKey = changeEvent.target.id;
-    if (changeEvent.target.checked) {
+    selectionHandler(changeEvent) {
+      const whichKey = changeEvent.target.id;
+      if (changeEvent.target.checked) {
         this.userSelections.push(whichKey);
-    } else {
+      } else {
         this.userSelections = this.userSelections.filter(function(value) {
-                return value != whichKey;
-        })
+          return value != whichKey;
+        });
+      }
     }
-  },
   },
   mounted() {
     AppDB.ref("workoutPrivate").on("child_added", this.dataHandler);
@@ -179,87 +239,86 @@ import { AppDB } from "../db-init.js";
     // AppDB.ref("workoutPublic").off("child_added", this.dataHandler);
     // AppDB.ref("workoutPublic").off("child_removed", this.removeExpenseItem);
   }
-  
-}
+};
 </script>
 
 <style>
 #page {
-    display: grid;
-    grid-template-rows: auto;
-    grid-template-columns: auto auto;
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: auto auto;
 }
 
 #workoutForm {
-    width: 40vw;
-    border-color: hsl(204, 9%, 62%);
-    border-style: solid;
-    border-radius: 15px;
-    padding: 2%;
-    display: grid;
-    margin: 3%;
-    grid-column-start: 1;
+  width: 40vw;
+  border-color: hsl(204, 9%, 62%);
+  border-style: solid;
+  border-radius: 15px;
+  padding: 2%;
+  display: grid;
+  margin: 3%;
+  grid-column-start: 1;
 }
 
 #table {
-    width: 60vw;
-    height: 335px;
-    border-color: hsl(204, 9%, 62%);
-    border-style: solid;
-    border-radius: 15px;
-    padding: 2%;
-    display: grid;
-    margin: 3%;
-    grid-column-start: 2;
+  width: 60vw;
+  height: 335px;
+  border-color: hsl(204, 9%, 62%);
+  border-style: solid;
+  border-radius: 15px;
+  padding: 2%;
+  display: grid;
+  margin: 3%;
+  grid-column-start: 2;
 }
 
 #publicTable {
-    width: 70vw;
-    height: 335px;
-    border-color: hsl(204, 9%, 62%);
-    border-style: solid;
-    border-radius: 15px;
-    padding: 2%;
-    display: grid;
-    margin: 3%;
-    grid-column-start: 2;
+  width: 70vw;
+  height: 335px;
+  border-color: hsl(204, 9%, 62%);
+  border-style: solid;
+  border-radius: 15px;
+  padding: 2%;
+  display: grid;
+  margin: 3%;
+  grid-column-start: 2;
 }
 
 #one {
-    grid-column-start: 2;
-    width: 30vw;
-    height: 30vh;
-    padding: 2%;
-    display: grid;
-    margin: 3%;
-    border-color: hsl(204, 9%, 62%);
-    border-style: solid;
-    border-radius: 15px;
+  grid-column-start: 2;
+  width: 30vw;
+  height: 30vh;
+  padding: 2%;
+  display: grid;
+  margin: 3%;
+  border-color: hsl(204, 9%, 62%);
+  border-style: solid;
+  border-radius: 15px;
 }
 
 #th {
-    background-color: hsl(204, 57%, 25%);
-    color: hsl(204, 57%, 92%);
+  background-color: hsl(204, 57%, 25%);
+  color: hsl(204, 57%, 92%);
 }
 
 h2 {
-    padding: 4%;
+  padding: 4%;
 }
 
 #amt {
-    text-align: right;
+  text-align: right;
 }
 
 #total {
-    text-align: right;
-    font-weight: bold;
+  text-align: right;
+  font-weight: bold;
 }
 
 #dataRows:nth-child(even) {
-    background-color: hsl(204, 57%, 56%);
+  background-color: hsl(204, 57%, 56%);
 }
 
 #dataRows:nth-child(odd) {
-    background-color: hsl(189, 62%, 69%);
+  background-color: hsl(189, 62%, 69%);
 }
 </style>
